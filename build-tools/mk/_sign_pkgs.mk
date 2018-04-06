3a37c7feed50cde3e2c5f619cb85c97a7c1b18d7
jmckenna <jason.mckenna@windriver.com>
2018-04-06 10:41:53 -0400

    Uprev grub2 and shim
    Uprev grub and shim packages to new versions.  The packages (grub
    in particullar) went through a major restructuring, including
    name change in output binary packages.  This commit updates
    build tools and files to be aware of new names and handle the
    new packages
    
    Change-Id: I89636218bdb8e97d39e69e764ae376b643657f50
    Depends-On: https://review.openstack.org/#/c/578440

--- build-tools/mk/_sign_pkgs.mk
+++ build-tools/mk/_sign_pkgs.mk
@@ -9,7 +9,7 @@ PKGS_LIST := $(wildcard *.rpm)
 # we need to skip the signature of some packages that
 # might be installed in file systems that do not support extended attributes
 # in the case of shim- and grub2-efi-, the UEFI configuration installs them in a VFAT file system
-PKGS_TO_SKIP := $(wildcard grub2-efi-[0-9]*.x86_64.rpm shim-[0-9]*.x86_64.rpm)
+PKGS_TO_SKIP := $(wildcard grub2-efi-[0-9]*.x86_64.rpm grub2-efi-x64-[0-9]*.x86_64.rpm shim-[0-9]*.x86_64.rpm shim-x64-[0-9]*.x86_64.rpm shim-ia32-[0-9]*.x86_64)
 
 PKGS_TO_SIGN = $(filter-out $(PKGS_TO_SKIP),$(PKGS_LIST))
 
