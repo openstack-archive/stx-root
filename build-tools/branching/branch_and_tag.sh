f4584afe7c859399720af3869d82213461ac1011
Sun Austin <austin.sun@intel.com>
2018-08-29 15:27:10 +0800

    Fix linters issues and enable tox/zuul linters job as gate
    Fix below issues:
        E003 Indent not multiple of 4
        E011 Then keyword is not on same line as if or elif keyword
        E020: Function declaration not in format "^function name {$": 'function check_all_explicit_deps_installed'
    
    add ignore in tox env
        E006 Line too long
        E010: Do not on same line as
    Change-Id: I13fb83452324c0db74c485f304827a6c9aacf9ae
    Story: 2003370
    Task: 24431
    Signed-off-by: Sun Austin <austin.sun@intel.com>

--- build-tools/branching/branch_and_tag.sh
+++ build-tools/branching/branch_and_tag.sh
@@ -10,30 +10,30 @@ NEW_TAG=vCGCS_DEV_0019
 OLD_BRANCH=CGCS_DEV_0018
 NEW_BRANCH=CGCS_DEV_0019
 
-if [ -z "$MY_REPO" ]; then 
-  echo "MY_REPO is unset"
-  exit 1
+if [ -z "$MY_REPO" ]; then
+    echo "MY_REPO is unset"
+    exit 1
 else
-  echo "MY_REPO is set to '$MY_REPO'"
+    echo "MY_REPO is set to '$MY_REPO'"
 fi
 
-if [ -d "$MY_REPO" ]; then 
-  cd $MY_REPO
-  echo "checking out and pulling old branch"
-  wrgit checkout $OLD_BRANCH
-  if [ $? -ne 0 ]; then
-    echo "ERROR: wrgit checkout $OLD_BRANCH"
-    exit 1
-  fi
+if [ -d "$MY_REPO" ]; then
+    cd $MY_REPO
+    echo "checking out and pulling old branch"
+    wrgit checkout $OLD_BRANCH
+    if [ $? -ne 0 ]; then
+        echo "ERROR: wrgit checkout $OLD_BRANCH"
+        exit 1
+    fi
 
-  wrgit pull
-  if [ $? -ne 0 ]; then
-    echo "ERROR: wrgit pull"
-    exit 1
-  fi
+    wrgit pull
+    if [ $? -ne 0 ]; then
+        echo "ERROR: wrgit pull"
+        exit 1
+    fi
 else
-  echo "Could not change to diectory '$MY_REPO'"
-  exit 1
+    echo "Could not change to diectory '$MY_REPO'"
+    exit 1
 fi
 
 echo "Finding subgits"
@@ -42,45 +42,35 @@ SUBGITS=`find . -type d -name ".git" | sed "s%/\.git$%%"`
 # Go through all subgits and create the NEW_BRANCH if it does not already exist
 # Go through all subgits and create the NEW_TAG if it does not already exist
 for subgit in $SUBGITS; do
- echo ""
- echo ""
- pushd $subgit > /dev/null
- git fetch
- git fetch --tags
- # check if destination branch already exists
- echo "$subgit"
- branch_check=`git branch -a --list $NEW_BRANCH`
- if [ -z "$branch_check" ]
- then
-   echo "Creating $NEW_BRANCH"
-   git checkout $OLD_BRANCH
-   git checkout -b $NEW_BRANCH
-   git push origin $NEW_BRANCH:$NEW_BRANCH
- else
-   echo "$NEW_BRANCH already exists"
- fi
- tag_check=`git tag -l $NEW_TAG`
- if [ -z "$tag_check" ]
- then
-   echo "Creating $NEW_TAG"
-   # create tag
-   git checkout $NEW_BRANCH
-   git pull origin
-   git tag $NEW_TAG
-   git push origin $NEW_TAG
- else
-   echo "$NEW_TAG already exists"
- fi
+    echo ""
+    echo ""
+    pushd $subgit > /dev/null
+    git fetch
+    git fetch --tags
+    # check if destination branch already exists
+    echo "$subgit"
+    branch_check=`git branch -a --list $NEW_BRANCH`
+    if [ -z "$branch_check" ]; then
+        echo "Creating $NEW_BRANCH"
+        git checkout $OLD_BRANCH
+        git checkout -b $NEW_BRANCH
+        git push origin $NEW_BRANCH:$NEW_BRANCH
+    else
+        echo "$NEW_BRANCH already exists"
+    fi
+    tag_check=`git tag -l $NEW_TAG`
+    if [ -z "$tag_check" ]; then
+        echo "Creating $NEW_TAG"
+       # create tag
+        git checkout $NEW_BRANCH
+        git pull origin
+        git tag $NEW_TAG
+        git push origin $NEW_TAG
+    else
+        echo "$NEW_TAG already exists"
+    fi
 
- popd > /dev/null
+    popd > /dev/null
 done
 
 echo "All done.  branches and tags are pushed"
-
-
-
-
-
-
-
-
--- build-tools/build_minimal_iso/build_centos.sh
+++ build-tools/build_minimal_iso/build_centos.sh
@@ -4,42 +4,42 @@
 
 CREATEREPO=$(which createrepo_c)
 if [ $? -ne 0 ]; then
-   CREATEREPO="createrepo"
+    CREATEREPO="createrepo"
 fi
 
 function final_touches {
    # create the repo
-   cd ${ROOTDIR}/${DEST}/isolinux
-   $CREATEREPO -g ../comps.xml .
-   
+    cd ${ROOTDIR}/${DEST}/isolinux
+    $CREATEREPO -g ../comps.xml .
+
    # build the ISO
-   printf "Building image $OUTPUT_FILE\n"
-   cd ${ROOTDIR}/${DEST}
-   chmod 664 isolinux/isolinux.bin
-   mkisofs -o $OUTPUT_FILE \
-      -R -D -A 'oe_iso_boot' -V 'oe_iso_boot' \
-      -b isolinux.bin -c boot.cat -no-emul-boot \
-      -boot-load-size 4 -boot-info-table \
-      -eltorito-alt-boot \
-      -e images/efiboot.img \
-            -no-emul-boot \
-      isolinux/   
+    printf "Building image $OUTPUT_FILE\n"
+    cd ${ROOTDIR}/${DEST}
+    chmod 664 isolinux/isolinux.bin
+    mkisofs -o $OUTPUT_FILE \
+        -R -D -A 'oe_iso_boot' -V 'oe_iso_boot' \
+        -b isolinux.bin -c boot.cat -no-emul-boot \
+        -boot-load-size 4 -boot-info-table \
+        -eltorito-alt-boot \
+        -e images/efiboot.img \
+        -no-emul-boot \
+        isolinux/
 
-   isohybrid --uefi $OUTPUT_FILE
-   implantisomd5 $OUTPUT_FILE
+    isohybrid --uefi $OUTPUT_FILE
+    implantisomd5 $OUTPUT_FILE
 
-   cd $ROOTDIR
+    cd $ROOTDIR
 }
 
 function setup_disk {
-	tar xJf emptyInstaller.tar.xz
-	mkdir ${DEST}/isolinux/Packages
+    tar xJf emptyInstaller.tar.xz
+    mkdir ${DEST}/isolinux/Packages
 }
 
 function install_packages {
-	cd ${DEST}/isolinux/Packages
-	ROOT=${ROOTDIR} ../../../cgts_deps.sh --deps=../../../${MINIMAL}
-	cd ${ROOTDIR}
+    cd ${DEST}/isolinux/Packages
+    ROOT=${ROOTDIR} ../../../cgts_deps.sh --deps=../../../${MINIMAL}
+    cd ${ROOTDIR}
 }
 
 
