8467ba9092ee44a11413757437b6bb751d902324
Paul-Emile Element <Paul-Emile.Element@windriver.com>
2018-09-07 13:09:07 -0400

    cleanup siging scripts the scripts contained hardcoded references to resources that are not visible outside of the environment where the scripts were originally creted and used
    The scripts sign-rpms was also updated with the original version
    that was intended to be submitted. The initial submission contained
    the wrong version.
    
    Closes-Bug: #1791343
    
    Change-Id: I8ce5884ad75156d3730cf30a451051d32445e136
    Signed-off-by: Paul-Emile Element <Paul-Emile.Element@windriver.com>

--- build-tools/sign_patch_formal.sh
+++ build-tools/sign_patch_formal.sh
@@ -13,21 +13,20 @@ fi
 
 PATCH_FILE_PATH=$1
 PATCH_FILE_NAME=$(basename ${PATCH_FILE_PATH})
-SIGNING_SERVER="signing@yow-tiks01"
 GET_UPLOAD_PATH="sudo /opt/signing/sign.sh -r"
 REQUEST_SIGN="sudo /opt/signing/sign_patch.sh"
 
 # Make a request for an upload path
 # Output is a path where we can upload stuff, of the form
 # "Upload: /tmp/sign_upload.5jR11pS0"
-UPLOAD_PATH=`ssh ${SIGNING_SERVER} ${GET_UPLOAD_PATH}`
+UPLOAD_PATH=`ssh ${SIGNING_USER}@${SIGNING_SERVER} ${GET_UPLOAD_PATH}`
 if [ $? -ne 0 ]; then
     echo "Could not get upload path.  Do you have permissions on the signing server?"
     exit 1
 fi
 UPLOAD_PATH=`echo ${UPLOAD_PATH} | cut -d ' ' -f 2`
 
-scp -q ${PATCH_FILE_PATH} ${SIGNING_SERVER}:${UPLOAD_PATH}
+scp -q ${PATCH_FILE_PATH} ${SIGNING_USER}@${SIGNING_SERVER}:${UPLOAD_PATH}
 if [ $? -ne 0 ]; then
     echo "Could upload patch"
     exit 1
@@ -36,14 +35,14 @@ echo "File uploaded to signing server"
 
 # Make the signing request.
 # Output is path of newly signed file
-RESULT=`ssh ${SIGNING_SERVER} ${REQUEST_SIGN} ${UPLOAD_PATH}/${PATCH_FILE_NAME}`
+RESULT=`ssh ${SIGNING_USER}@${SIGNING_SERVER} ${REQUEST_SIGN} ${UPLOAD_PATH}/${PATCH_FILE_NAME}`
 if [ $? -ne 0 ]; then
     echo "Could not perform signing -- output $RESULT"
     exit 1
 fi
 
 echo "Signing complete.  Downloading"
-scp -q ${SIGNING_SERVER}:${RESULT} ${PATCH_FILE_PATH}
+scp -q ${SIGNING_USER}@${SIGNING_SERVER}:${RESULT} ${PATCH_FILE_PATH}
 if [ $? -ne 0 ]; then
     echo "Could not download newly signed file"
     exit 1
