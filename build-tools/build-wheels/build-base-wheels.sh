1a3090b5b720d2a13ba9d675f03505f58803b09a
Don Penney <don.penney@windriver.com>
2018-10-22 16:13:17 -0400

    Add tools to build python wheels and wheel tarball
    This update adds tools for building wheels for upstream python
    modules, and generating a StarlingX wheels tarball.
    
    Building upstream python modules is done within a docker container,
    with a wheels.cfg configuration file that identifies the upstream
    sources and expected python wheel output. The build-base-wheels.sh
    tool will create the wheel builder image, launch it, and run the
    build script.
    
    The get-stx-wheels.sh tool will extract the python wheels built by
    the StarlingX load.
    
    The build-wheel-tarball.sh script will combine the wheels from the
    base and StarlingX wheel sets into a single tarball that can be used
    when building container images for StarlingX services.
    
    Story: 2003907
    Task: 27531
    Change-Id: Icf6c63699a635d1173dcef9eb1d12ae7c4e39108
    Signed-off-by: Don Penney <don.penney@windriver.com>

---
+++ build-tools/build-wheels/build-base-wheels.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+#
+# Copyright (c) 2018 Wind River Systems, Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# This utility sets up a docker image to build wheels
+# for a set of upstream python modules.
+#
+
+# Required env vars
+if [ -z "${MY_WORKSPACE}" -o -z "${MY_REPO}" ]; then
+    echo "Environment not setup for builds" >&2
+    exit 1
+fi
+
+DOCKER_PATH=${MY_REPO}/build-tools/build-wheels/docker
+WHEELS_CFG=${DOCKER_PATH}/wheels.cfg
+KEEP_IMAGE=no
+KEEP_CONTAINER=no
+OS=centos
+
+function usage {
+    cat >&2 <<EOF
+Usage:
+$(basename $0) [ --os <os> ] [ --keep-image ] [ --keep-container ]
+
+Options:
+    --os:             Specify base OS (eg. centos)
+    --keep-image:     Skip deletion of the wheel build image in docker
+    --keep-container: Skip deletion of container used for the build
+
+EOF
+}
+
+OPTS=$(getopt -o h -l help,os:,keep-image,keep-container -- "$@")
+if [ $? -ne 0 ]; then
+    usage
+    exit 1
+fi
+
+eval set -- "${OPTS}"
+
+while true; do
+    case $1 in
+        --)
+            # End of getopt arguments
+            shift
+            break
+            ;;
+        --os)
+            OS=$2
+            shift 2
+            ;;
+        --keep-image)
+            KEEP_IMAGE=yes
+            shift
+            ;;
+        --keep-container)
+            KEEP_CONTAINER=yes
+            shift
+            ;;
+        -h | --help )
+            usage
+            exit 1
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+BUILD_OUTPUT_PATH=${MY_WORKSPACE}/std/build-wheels-${OS}/base
+BUILD_IMAGE_NAME="${USER}-$(basename ${MY_WORKSPACE})-wheelbuilder:${OS}"
+
+DOCKER_FILE=${DOCKER_PATH}/${OS}-dockerfile
+
+function supported_os_list {
+    for f in ${DOCKER_PATH}/*-dockerfile; do
+        echo $(basename ${f%-dockerfile})
+    done | xargs echo
+}
+
+if [ ! -f ${DOCKER_FILE} ]; then
+    echo "Unsupported OS specified: ${OS}" >&2
+    echo "Supported OS options: $(supported_os_list)" >&2
+    exit 1
+fi
+
+#
+# Check build output directory for unexpected files,
+# ie. wheels from old builds that are no longer in wheels.cfg
+#
+if [ -d ${BUILD_OUTPUT_PATH} ]; then
+
+    for f in ${BUILD_OUTPUT_PATH}/*; do
+        grep -q "^$(basename $f)|" ${WHEELS_CFG}
+        if [ $? -ne 0 ]; then
+            echo "Deleting stale file: $f"
+            rm -f $f
+        fi
+    done
+else
+    mkdir -p ${BUILD_OUTPUT_PATH}
+    if [ $? -ne 0 ]; then
+        echo "Failed to create directory: ${BUILD_OUTPUT_PATH}" >&2
+        exit 1
+    fi
+fi
+
+# Create the builder image
+docker build -t ${BUILD_IMAGE_NAME} -f ${DOCKER_PATH}/${OS}-dockerfile ${DOCKER_PATH}
+if [ $? -ne 0 ]; then
+    echo "Failed to create build image in docker" >&2
+    exit 1
+fi
+
+# Run the image, executing the build-wheel.sh script
+RM_OPT=
+if [ "${KEEP_CONTAINER}" = "no" ]; then
+    RM_OPT="--rm"
+fi
+docker run ${RM_OPT} -v ${BUILD_OUTPUT_PATH}:/wheels -i -t ${BUILD_IMAGE_NAME} /docker-build-wheel.sh
+
+if [ "${KEEP_IMAGE}" = "no" ]; then
+    # Delete the builder image
+    echo "Removing docker build image ${BUILD_IMAGE_NAME}"
+    docker image rm ${BUILD_IMAGE_NAME}
+    if [ $? -ne 0 ]; then
+        echo "Failed to delete build image from docker" >&2
+    fi
+fi
+
+# Check for failures
+if [ -f ${BUILD_OUTPUT_PATH}/failed.lst ]; then
+    # Failures would already have been reported
+    exit 1
+fi
+
